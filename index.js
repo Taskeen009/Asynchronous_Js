6c3e3f81259a536e93c5a2ec344167d0f97ca4ab
Taskeen <taskeen.fa@gmail.com>
2024-11-17 06:11:22 +0530

    promise

--- index.js
+++ index.js
@@ -25,19 +25,19 @@
 
 
 
-function printHello(callback) {
-    setTimeout(() => {
-        console.log("hello");
-        callback(); // Execute the callback if provided
-    }, 1000);
-}
+// function printHello(callback) {
+//     setTimeout(() => {
+//         console.log("hello");
+//         callback(); // Execute the callback if provided
+//     }, 1000);
+// }
 
-function printBye(callback) {
-    setTimeout(() => {
-        console.log("bye");
-         callback(); // Execute the callback if provided
-    }, 1000);
-}
+// function printBye(callback) {
+//     setTimeout(() => {
+//         console.log("bye");
+//          callback(); // Execute the callback if provided
+//     }, 1000);
+// }
 
 // printHello(() => {
 //     console.log("This runs after hello.");
@@ -47,8 +47,30 @@ function printBye(callback) {
 //     console.log("This runs after bye.");
 // });
 
-printHello(() => {
-    printBye(() => {
-        console.log("Done!");
-    });
-});
+// printHello(() => {
+//     printBye(() => {
+//         console.log("Done!");
+//     });
+// });
+
+
+
+////////////////////
+
+
+const showmsg=(cb)=>{
+    console.log(cb)
+}
+
+const firstmsg=(cb)=>{
+    setTimeout(()=>{
+        showmsg("hello")
+        cb()
+    },2000)
+}
+
+const secondmsg=()=>{
+    showmsg("world");
+}
+
+firstmsg(secondmsg)
---
+++ promise.js
@@ -0,0 +1,5 @@
+//promise
+
+
+
+const 
