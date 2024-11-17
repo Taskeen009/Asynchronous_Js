b510c62b40b07aad6d774db943f339a0cbf37e9c
Taskeen <taskeen.fa@gmail.com>
2024-11-17 05:59:29 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+// setTimeout(()=>{
+//     console.log("hello")
+// },1000)
+
+// setTimeout(()=>{
+//     console.log("bye");
+// },1000)
+
+
+// function printHello() {
+//     setTimeout(() => {
+//         console.log("hello");
+//     }, 1000);
+// }
+
+// function printBye() {
+//     setTimeout(() => {
+//         console.log("bye");
+//     }, 2000);
+// }
+
+
+// printBye();
+// printHello();
+
+
+
+function printHello(callback) {
+    setTimeout(() => {
+        console.log("hello");
+        callback(); // Execute the callback if provided
+    }, 1000);
+}
+
+function printBye(callback) {
+    setTimeout(() => {
+        console.log("bye");
+         callback(); // Execute the callback if provided
+    }, 1000);
+}
+
+// printHello(() => {
+//     console.log("This runs after hello.");
+// });
+
+// printBye(() => {
+//     console.log("This runs after bye.");
+// });
+
+printHello(() => {
+    printBye(() => {
+        console.log("Done!");
+    });
+});
